151cdcfdb9682b3aaec2f42c8847ea353138e774
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2023-11-26 09:20:26 -0300

    FEAT: Add repository for users

---
+++ src/repositories/users-repository.ts
@@ -0,0 +1,5 @@
+import { Prisma, Users } from "@prisma/client";
+export interface UsersRepository {
+    create(data: Prisma.UsersCreateInput): Promise<Users>;
+    findByEmail(email: string): Promise<Users | null>
+}

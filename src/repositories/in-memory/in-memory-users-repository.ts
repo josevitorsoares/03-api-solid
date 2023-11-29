b336def2fbd1b8ae9dfd36d3861e77e75ccd0fc2
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2023-11-29 06:52:15 -0300

    FEAT: Implementing findById method

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -3,7 +3,8 @@ import { UsersRepository } from "../users-repository";
 
 export class InMemoryUsersRepository implements UsersRepository {
     public items: Users[] = [];
-
+    
+    
     async create(data: Prisma.UsersCreateInput) {
         const user = {
             id: "user-1",
@@ -12,13 +13,22 @@ export class InMemoryUsersRepository implements UsersRepository {
             password_hash: data.password_hash,
             created_at: new Date()
         };
-
+        
         this.items.push(user);
-
+        
         return user;
     }
     async findByEmail(email: string) {
         const user = this.items.find((item) => item.email === email);
+
+        if (!user) {
+            return null;
+        }
+        
+        return user;
+    }
+    async findById(id: string): Promise<{ id: string; name: string; email: string; password_hash: string; created_at: Date; } | null> {
+        const user = this.items.find((item) => item.id === id);
 
         if (!user) {
             return null;

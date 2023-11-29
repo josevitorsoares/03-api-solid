73b4e4a1b91c705fc43b92f0919a55abcdb076f9
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2023-11-29 06:50:57 -0300

    FEAT: Add findById method

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -2,4 +2,5 @@ import { Prisma, Users } from "@prisma/client";
 export interface UsersRepository {
     create(data: Prisma.UsersCreateInput): Promise<Users>;
     findByEmail(email: string): Promise<Users | null>
+    findById(id: string): Promise<Users | null>
 }

8768b631b4f0c9b99c6a234468c5c577942354be
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2023-12-03 07:22:34 -0300

    REFACTIR: Add await in promise

--- src/use-cases/get-user-profile.spec.ts
+++ src/use-cases/get-user-profile.spec.ts
@@ -29,10 +29,10 @@ describe("Get User Profile Use Case", () => {
     });
 
     it("should not be able to get user profile with wrong id", async () => {
-        expect(() => {
+        await expect(() => 
             sut.execute({
                 userId: "non-existing-id"
-            });
-        }).rejects.toBeInstanceOf(ResourceNotFoundError);
+            })
+        ).rejects.toBeInstanceOf(ResourceNotFoundError);
     });
 });

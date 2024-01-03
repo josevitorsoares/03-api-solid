3295fd3114f8a4f03d1693ef8574e72c3cb804e6
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2024-01-03 20:15:25 -0300

    FEAT: Add method fetchManyByUserId

--- src/repositories/check-in-repository.ts
+++ src/repositories/check-in-repository.ts
@@ -3,4 +3,5 @@ import { Prisma, CheckIn } from "@prisma/client";
 export interface CheckInsRepositoy {
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
+    fetchManyByUserId(userId: string, page: number): Promise<CheckIn[]>
 }

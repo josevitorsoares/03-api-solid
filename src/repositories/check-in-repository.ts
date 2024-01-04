463a76a0d349ba0d267ee1a8b7d4038ef603b9b5
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2024-01-04 06:49:52 -0300

    FEAT: Add method countByUserId

--- src/repositories/check-in-repository.ts
+++ src/repositories/check-in-repository.ts
@@ -3,5 +3,6 @@ import { Prisma, CheckIn } from "@prisma/client";
 export interface CheckInsRepositoy {
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
-    fetchManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+    fetchManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
+    countByUserId(userId: string): Promise<number>;
 }
